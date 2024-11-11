b5bc7cc9187ad9ec3de48d41bba5bc7ad720b095
jatebezi <jempire47@gmial.com>
2024-11-11 21:50:12 +0000

    my source code

--- s3.tf
+++ s3.tf
@@ -3,3 +3,13 @@ resource "aws_s3_bucket" "yan6464" {
     tags = "hydbd"
     
 }
+
+resource "aws_ec2_instance" "man" {
+    ami = "ami-0c94855ba95b798c7"
+    instance_type = "t2.micro"
+    tags ={
+    
+        Name = "man"
+    }
+  
+}
